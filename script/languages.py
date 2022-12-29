50b0304d9df1573afbe811d9a08c8884ea732b08
Aum Patel <73774338+patel-aum@users.noreply.github.com>
2022-12-29 19:58:27 +0530

    Update languages.py
    This modified version of the script uses the redis library to connect to a Redis cache and store the data retrieved from the GitHub API. When the script is ran, it first checks the cache to see if the data for the specified tag is already stored there. If the data is found in the cache, it is loaded into memory and used by the script. If the data is not found in the cache, it is fetched from the GitHub API and then stored in the cache for future use. This can significantly speed up the script if it is run multiple times with the same tag, as it avoids the need to make a request to the GitHub API each time.

--- script/languages.py
+++ script/languages.py
@@ -1,18 +1,30 @@
 """Helper script to update language list from the frontend source."""
 import json
-from pathlib import Path
 import sys
 
 import requests
+import redis
 
 from .hassfest.serializer import format_python_namespace
 
+# Connect to the Redis cache
+cache = redis.Redis()
+
 tag = sys.argv[1] if len(sys.argv) > 1 else "dev"
 
-req = requests.get(
-    f"https://raw.githubusercontent.com/home-assistant/frontend/{tag}/src/translations/translationMetadata.json"
-)
-data = json.loads(req.content)
+# Check the cache for the data
+data = cache.get(tag)
+
+if data is None:
+    # Data not found in cache, fetch it from the GitHub API
+    req = requests.get(
+        f"https://raw.githubusercontent.com/home-assistant/frontend/{tag}/src/translations/translationMetadata.json"
+    )
+    data = json.loads(req.content)
+
+    # Store the data in the cache for future use
+    cache.set(tag, data)
+
 languages = set(data.keys())
 
 Path("homeassistant/generated/languages.py").write_text(
